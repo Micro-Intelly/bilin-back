2541f056d5073fa516f7665074da717115a4cd82
Wei-bin-Wu <weibin789123@gmail.com>
2022-09-26 21:45:51 +0200

    First test return response json of rest api

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,9 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\PostResource;
+use App\Models\Post;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
-    //
+    public function testGet($id){
+        return new PostResource(Post::findOrFail($id));
+    }
 }
